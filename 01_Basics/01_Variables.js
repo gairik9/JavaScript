7e01b98c04d78152791635491e43c96d68be5a0d
Gairik Chakraborty <42964943+gairik9@users.noreply.github.com>
2023-09-13 18:18:48 +0000

    JS - Constants and Variables Done.

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,25 @@
+// Introduction To Variables :
+
+// Declaring Variables :
+
+var accountId = 12345611234; // Not Recommended Anymore.
+
+let phoneNumber = 7002331231; // Recommended.
+
+let accountState;
+
+emailId = 'ghost@proton.me'; // Not Recommended.
+
+// Declaring Constants :
+
+const password = "@qwerty1fgh";
+
+// password = 34212356; // Re-assignment to const type is not allowed.
+
+// console.log(accountId);
+// console.log(phoneNumber);
+// console.log(password);
+
+// OR
+
+console.table([accountId, phoneNumber, password, emailId, accountState])
