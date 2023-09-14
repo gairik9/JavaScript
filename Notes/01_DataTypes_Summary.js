40f436d5989139edb3b81ad973cd364c8e37f0f8
Gairik Chakraborty <42964943+gairik9@users.noreply.github.com>
2023-09-14 14:50:37 +0000

    JS - DataTypes Summary Notes Created.

---
+++ Notes/01_DataTypes_Summary.js
@@ -0,0 +1,18 @@
+// PRIMITIVE TYPES :
+
+// Total 7 Types :
+
+// 1. String
+// 2. Number
+// 3. Null
+// 4. Undefined
+// 5. Symbol
+// 6. BigInt
+// 7. Boolean
+
+// REFERENCE (OR NON-PRIMITIVE) TYPES :
+
+// 1. Arrays
+// 2. Objects
+// 3. Functions
+
