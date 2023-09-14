78e7402df7ba86feb5e821024feb62f40c16ebf7
Gairik Chakraborty <42964943+gairik9@users.noreply.github.com>
2023-09-14 14:07:20 +0000

    JS - Comparisons Done.

---
+++ 01_Basics/04_Comparison.js
@@ -0,0 +1,23 @@
+// JavaScript Comparisons :
+
+console.log(2 > 1);
+console.log(1 >= 1);
+console.log(10 < 5);
+console.log(10 ==10);
+
+console.log("10" ==10);
+console.log("10" ===10);
+
+console.log(null < 0);
+console.log(null > 0);
+console.log(null <= 0);
+console.log(null >=0);
+console.log(null == 0);
+console.log(undefined < 0);
+console.log(undefined > 0);
+console.log(undefined <= 0);
+console.log(undefined >=0);
+console.log(undefined == 0);
+
+
+
