e980e3469200141c073dfd07c8ff4452d78e73d5
Gairik Chakraborty <42964943+gairik9@users.noreply.github.com>
2023-09-14 15:12:04 +0000

    JS - Stacks and Heap Basics.

---
+++ Notes/02_Memory.js
@@ -0,0 +1,8 @@
+// Memory - STACK And HEAP Memory :
+
+/*
+1. All the primitive datatypes use Stack and all the non-primitive types use Heap memory. 
+
+2. The stack is used to store static data, whereas the heap is used to store dynamic data.
+*/
+
