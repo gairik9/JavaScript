b5aa9c7b8a90f91bf2252521088ba68ac2551a92
Gairik Chakraborty <42964943+gairik9@users.noreply.github.com>
2023-09-19 18:54:01 +0000

    JS - Arrays Basics Done Partially.

---
+++ 01_Basics/08_Arrays.js
@@ -0,0 +1,19 @@
+// JavaScript Arrays :
+
+let arrOne = [1,2,3,4,5];
+let arrTwo = ['A','B','C'];
+
+// console.log(arrOne);
+// console.log(`Element at position 0 is ${arrOne[0]}`)
+// console.log(arrTwo);
+
+// arrOne.push(6)
+// console.log(arrOne);
+
+// arrOne.pop()
+// console.log(arrOne);
+
+// console.log(arrTwo.includes('A'))
+// console.log(arrTwo.indexOf('C'))
+// console.log(arrTwo.concat(arrOne));
+
