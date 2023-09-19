49d91f1520d32259b31d3a0ab1ce7b51a7984119
Gairik Chakraborty <42964943+gairik9@users.noreply.github.com>
2023-09-19 16:22:59 +0000

    JS - Numbers _ Math Done.

---
+++ 01_Basics/06_Numbers_And_Math.js
@@ -0,0 +1,31 @@
+// JavaScript Numbers And Math :
+
+// Numbers :
+
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+// console.log(typeof(balance.toString()));
+
+// const marks = 20.991;
+// console.log(marks.toFixed(2));
+
+// const num = 100000000;
+// console.log(num.toLocaleString())
+// console.log(num.toLocaleString('en-IN'))
+
+// Math :
+
+// console.log(Math.abs(-20));
+// console.log(Math.round(3.14));
+// console.log(Math.ceil(3.14));
+// console.log(Math.floor(3.14));
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.round(Math.random()*10));
+console.log(Math.round((Math.random()*10)+1));
+
+
