812feca70e9b57f7dd0be299a4b0bd10f9299e6d
Gairik Chakraborty <42964943+gairik9@users.noreply.github.com>
2023-09-23 10:19:49 +0000

    JS : Objects Introduction

---
+++ 01_Basics/09_Objects.js
@@ -0,0 +1,19 @@
+// JavaScript Objects :
+
+const user = {
+    name : "Elon Mask",
+    username : "ghost",
+    address : "LA",
+    phone : "333-091234897",
+    availability : ['Mon', 'Tue', 'Wed'],
+};
+
+// Accessing Objects :
+console.log(user.name);
+// OR
+console.log(user['username']);
+
+// Changing Property :
+user.name = 'Mark Zuckerberg';
+console.log(user)
+
