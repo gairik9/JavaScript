f2489bcb694c6840f160c2b2648ce1d83c18c2be
Gairik Chakraborty <42964943+gairik9@users.noreply.github.com>
2023-09-23 17:29:20 +0000

    JS : Control Flow - if, if-else,if...else-if...else

---
+++ 02_Intermediate/04_ControlFLow.js
@@ -0,0 +1,32 @@
+// JavaScript Control Flow :
+
+// If Statement :
+// One :
+// if(true){
+//     console.log('Welcome To JS Control Flow');
+// }
+
+// Two :
+// const isLoggedIn = true;
+// if(isLoggedIn === true){
+//     console.log('User is logged in.')
+// }
+
+// Three :
+// let temperature = 30;
+// if(temperature >= 30){
+//     console.log('Temperature is high.')
+// }else{
+//     console.log('Temperature is low.')
+// }
+
+// Four :
+// let temperature = 21;
+// if(temperature <= 20){
+//     console.log('Temperature is Moderate.')
+// }else if(temperature >=21){
+//     console.log("Temperature is High.")
+// }
+// else{
+//     console.log('Temperature is Low.')
+// }
