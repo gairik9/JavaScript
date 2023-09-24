407512fab72c8c842f640ec82df4e98ff7a6bfea
Gairik Chakraborty <42964943+gairik9@users.noreply.github.com>
2023-09-24 14:26:20 +0000

    Data Types Final.

--- 01_Basics/02_DataTypes.js
+++ 01_Basics/02_DataTypes.js
@@ -1,5 +1,22 @@
 // JavaScript DataTypes :
 
+/* -------------------------------------------------------------------------
+DataTypes:
+1. Null - It is a special type that represents an empty or unknown value.
+
+2. BigInt - An integer with arbitrary precision (Can be used to strore large integers, even beyond safe integer limit.[Safe Ineteger Limit is 2(53)-1, here 53 is the power.]).
+
+3. Undefined - When the value of the variable is not defined.
+
+4. Symbol -
+
+5. Objects - Covered In A Different File.
+
+6. Strings - Covered In A Different File.
+
+7. Numbers - Simple Integers or Floating Point Integers.
+------------------------------------------------------------------------- */
+
 "use strict"; //Treats All JS Code As Newer Version.
 
 let accNumber = 112345112344; // Number Type.
@@ -8,19 +25,27 @@ let accHolderName = 'John Wick'; // String Type.
 
 let isAccActive = true; // Boolean Type.
 
-console.table([accNumber, accHolderName, isAccActive])
+let largeNumber = 9999999999n; //Assign 'n' after a number to create a BigInt integer. 
 
-/* 
-Other DataTypes:
-1. null - It is a special value that represents an empty or unknown value.
-2. BigInt - An integer with arbitrary precision.
-3. undefined - When the value of the variable is not defined.
-4. symbol
-5. Objects
-*/
+let emptyVariable; // Undefined.
 
-console.log(typeof accNumber)
-console.log(typeof accHolderName)
-console.log(typeof isAccActive)
-console.log(typeof null)  // Type is "object".
-console.log(typeof undefined) // Type is "undefined".
+let nullExample = null;
+
+console.table([accNumber, accHolderName, isAccActive, largeNumber, emptyVariable, nullExample])
+
+// console.log(typeof accNumber) // Type is number.
+
+// console.log(typeof accHolderName) // Type is string.
+
+// console.log(typeof isAccActive) // Type is boolean.
+
+// console.log(typeof null)  // Type is "object".
+
+// console.log(typeof undefined) // Type is "undefined".
+
+// console.log(typeof BigInt) // Type is "function".
+
+// console.log(typeof largeNumber); // Type is "BigInt".
+
+// console.log(typeof nullExample); // Type is "Object".
+
