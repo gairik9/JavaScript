a495b9431f051121417d2901ab29236c2a729b08
Gairik Chakraborty <42964943+gairik9@users.noreply.github.com>
2023-09-26 18:38:48 +0000

    JS - Math Object Done.

--- 01_Basics/06_Numbers_And_Math.js
+++ 01_Basics/06_Numbers_And_Math.js
@@ -1,7 +1,7 @@
 // JavaScript Numbers And Math :
 
 let score = 100;
-// console.log(typeof score);
+console.log(typeof score);
 
 
 // JS Math Object :
@@ -10,4 +10,23 @@ let score = 100;
 // Examples : 
 
 console.log(Math.round(9.841)); // Rounds A Number To It's Nearest Whole Number.
+
+console.log(Math.trunc(414.5345334)); // Truncate Method Returns Only The Integer Part.
+
+console.log(Math.pow(2,4)) // Returns X To The Power Y.
+
+console.log(Math.abs(-232.4434)); // Returns The Absolute Positive Values.
+
+console.log(Math.min(131,43,64,121)); // Returns The Smallest Value.
+
+console.log(Math.max(131,43,64,121)); // Returns The Greatest Value.
+
+// Math.random() : ----------------------------------------
+
+console.log(Math.random()*10) // Prints A Number B/w 0 To 10;
+
+console.log((Math.random()*10)+1) // Prints A Number B/w 1 To 10;
+
+console.log(Math.trunc((Math.random()*10)+1)) // Prints A Number B/w 1 To 10;
+
 
